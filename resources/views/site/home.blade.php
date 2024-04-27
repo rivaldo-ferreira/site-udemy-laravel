025beb8dff1de3d1fd45b79dd07335c1b22896a1
Rivas <rivaldo@gmail.com>
2024-04-27 19:52:35 -0300

    parte001

---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,10 @@
+@extends('site.layout')
+@section('title', 'Home')
+@section('content')
+
+    <row class="container">
+        <h1 class="flow-text">Página Home</h1>
+        <p>Teste de conteúdo</p>
+    </row>
+
+@endsection
--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -9,12 +9,23 @@
 
     {{-- css e javascript --}}
     @vite(['resources/css/app.css', 'resource/js/app.js'])
+
+    {{-- css materialize --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
 </head>
 
 <body>
 
     {{-- conteudo principal --}}
     @yield('content')
+
+
+    {{-- jquery --}}
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
+    {{-- javascript materialize --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
 </body>
 
