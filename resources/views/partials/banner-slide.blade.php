fd8040ddf3d1a6f58be03495b2cf0b70992828fb
Rivas <rivaldo@gmail.com>
2024-04-29 19:19:42 -0300

    formulário modal

--- resources/views/partials/banner-slide.blade.php
+++ resources/views/partials/banner-slide.blade.php
@@ -1,7 +1,7 @@
 <div class="slider">
     <ul class="slides">
         <li>
-            <img src="{{ asset('images/slider-1.jpg') }}" alt="Imagem Work">
+            <img src="{{ asset('images/slider-1.jpg') }}" class="responsive-img" alt="Imagem Work">
             <div class="caption left-align">
                 <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
                 <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi dicta earum consequatur
@@ -13,7 +13,7 @@
             </div>
         </li>
         <li>
-            <img src="{{ asset('images/slider-2.jpg') }}" alt="Imagem Work">
+            <img src="{{ asset('images/slider-2.jpg') }}" class="responsive-img" alt="Imagem Work">
             <div class="caption right-align">
                 <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
                 <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi dicta earum consequatur
@@ -30,6 +30,35 @@
 {{-- janela modal --}}
 <div class="modal" id="modal-lead">
     <div class="modal-content">
-        <h3>Janela Modal</h3>
+        <h3 class="flow-text">Preencha o formulário</h3>
+
+        <form action="" method="post">
+            @csrf
+            <div class="row">
+                {{-- campo nome --}}
+                <div class="col s12 m6 input-field">
+                    <i class="material-icons prefix">person</i>
+                    <input type="text" name="name" id="name" required>
+                    <label for="name">Digite seu nome:</label>
+                </div>
+                {{-- campo telefone --}}
+                <div class="col s12 m6 input-field">
+                    <i class="material-icons prefix">phone_iphone</i>
+                    <input type="tel" name="tel" id="tel" required>
+                    <label for="tel">Digite seu telefone:</label>
+                </div>
+                {{-- campo email --}}
+                <div class="col s12 input-field">
+                    <i class="material-icons prefix">email</i>
+                    <input type="email" name="email" id="email" required>
+                    <label for="email">Digite seu e-mail:</label>
+                </div>
+                {{-- botoes --}}
+                <div class="col s12 m6 input-field">
+                    <input type="submit" value="Enviar" class="btn btn-small blue">
+                    <input type="reset" value="Apagar" class="btn btn-small red">
+                </div>
+            </div>
+        </form>
     </div>
 </div>
